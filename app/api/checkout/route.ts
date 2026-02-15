ad4b160ca7dc389a1121525894fa5bb8d3528f98
adnanahmad55 <adnanahmed5575@gmail.com>
2026-02-15 21:16:20 +0530

    changes

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
+//const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   try {
